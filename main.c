bd67bfa61cd3df215e5dcae49555afb7afcbb2e7
AJ Iyanu <aderemijoseph@live.com>
2023-10-11 03:37:49 +0100

    fixed memory leaks with execute function, changed prompt head and added an exit command

--- main.c
+++ main.c
@@ -1,42 +1,48 @@
 #include "main.h"
 
 /**
-* main - main function that runs all other functions
-* @argc: number of command line arguments
-* @argv: array of pointers holding the commands passed to terminal
-* @env: array of pointers for environmental variable
-* Return: 1 if successful
-*/
+ * main - main function that runs all other functions
+ * @argc: number of command line arguments
+ * @argv: array of pointers holding the commands passed to terminal
+ * @env: array of pointers for environmental variable
+ * Return: 1 if successful
+ */
+
 int main(int argc, char **argv, char **env)
 {
 	/* gets the env path */
-	for (;;)
+	for (int cmdstatus = 0; cmdstatus == 0;)
 	{
 		char *lineptr = NULL, **splits;
-		size_t n = 0;
-		ssize_t count;
+		char *prompt = "KingO $ ";
+		size_t n = 0, output_check = -1, count = 0;
 		(void) argc;
 		(void) argv;
 
 		env = environ;
+		if (cmdstatus == 1)
+			break;
 
 		if (isatty(STDIN_FILENO))
 		{
-			printf("($) ");
-		}
-		count = getline(&lineptr, &n, stdin);
-
-		if (count == -1)
-		{
-			free(lineptr);
-			exit(0);
-		}
-		splits = split_commands(lineptr);
-		execute(splits, env);
-		free(lineptr);
-		if (!isatty(STDIN_FILENO))
-		{
+			for (int p = 0; p < 8; p++)
+				putchar(prompt[p]);
+			count = getline(&lineptr, &n, stdin);
+			if (count == output_check)
+				free(lineptr), free(splits), n = 0, exit(0);
+			splits = split_commands(lineptr), free(lineptr);
+			cmdstatus = execute(splits, env);
 			cleanup(splits);
+		/*	free(lineptr);*/
+		}
+		else
+		{
+			count = getline(&lineptr, &n, stdin);
+			if (count == output_check)
+				free(lineptr), exit(0);
+			splits = split_commands(lineptr), free(lineptr), execute(splits, env);
+			cleanup(splits);
+/*			free(lineptr);*/
 		}
 	}
 	return (0);
